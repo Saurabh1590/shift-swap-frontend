966946458c6016abe8e92ad012eb07de1ae31264
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-08-12 18:11:29 +0530

    minor fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production'
-  ? 'https://shift-swap-backend.onrender.com/api' // <-- Paste your Render backend URL here
+  ? 'https://shift-swap-backend.onrender.com/' // <-- Paste your Render backend URL here
   : 'http://localhost:5000/api';
 
 const api = axios.create({
